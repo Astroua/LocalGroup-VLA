ca2241c210fc7cd91c664d364d7d2405229e226a
e-koch <koch.eric.w@gmail.com>
2019-03-19 17:27:51 -0400

    Initial 11B cleaning scripts

---
+++ 11B-124/imaging/line_imaging_stage1.py
@@ -0,0 +1,86 @@
+
+'''
+Create a dirty for the given SPW
+
+Should be run from the M31_imaging/11B-124 folder on cedar
+
+Stage 1 cleaning - no clean mask down to 5-sigma
+'''
+
+import os
+
+# Requires analysisutils to be appended to the casa path
+# Load in the auto image parameter setters
+from CASA_functions import set_cellsize, set_imagesize
+
+from tasks import tclean, listobs
+
+spw_num = 0
+
+output_path = "HI_stage1"
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+# Grab all of the MS tracks in the folder (should be 17)
+myvis = "11B-124_HI_spw_0_LSRK.mms.contsub"
+
+# Run listobs
+listobs(vis=myvis)
+
+# Assume we can set reasonable image parameters from any of the tracks
+mycellsize = set_cellsize(myvis, spw_num, sample_factor=6.,
+                          baseline_percentile=95,
+                          return_type="str")
+casalog.post("Cell size: {}".format(mycellsize))
+
+# Choose something low as there is some HI near the edges of the mosaic
+mypblimit = 0.05
+
+# Will look for all M33 fields and assume they are all used in the mosaic
+source = 'M31'
+
+myimagesize = set_imagesize(myvis, spw_num, source, sample_factor=6.,
+                            max_size=15000, pblevel=mypblimit)
+casalog.post("Image size: {}".format(myimagesize))
+
+# Image ALL channels in the MS. Just looking for reduction issues
+default('tclean')
+
+# Don't image the channel edges
+# pad_chan = int(np.ceil(linespw_dict[spw_num][2] * 0.05))
+# num_chan = int(linespw_dict[spw_num][2]) - 2 * pad_chan
+
+tclean(vis=myvis,
+       datacolumn='corrected',
+       imagename=os.path.join(output_path,
+                              'M31_11B-124_{0}_spw_{1}.dirty'
+                              .format("HI", spw_num)),
+       spw=str(spw_num),
+       field='M31*',
+       imsize=myimagesize,
+       cell=mycellsize,
+       specmode='cube',
+       start=1,
+       width=1,
+       nchan=-1,
+       startmodel=None,
+       gridder='mosaic',
+       weighting='natural',
+       niter=1000000,
+       threshold='',
+       nsigma=5.,
+       phasecenter='J2000 00h42m44.350 +41d16m08.63',
+       restfreq="1.420405752GHz",
+       outframe='LSRK',
+       pblimit=mypblimit,
+       usemask='pb',
+       mask=None,
+       deconvolver='multiscale',
+       scales=[0, 5, 10, 50],
+       pbcor=False,
+       veltype='radio',
+       chanchunks=-1,
+       restoration=True,
+       parallel=True,
+       )
---
+++ 11B-124/imaging/line_imaging_stage1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --time=124:00:00
+#SBATCH --mem=1547000M
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --job-name=M31_11B_stage1clean_cube-%J
+#SBATCH --output=casa-m31_11B_stage1clean_cube-%J.out
+export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE
+
+module restore my_default
+
+source /home/ekoch/.bashrc
+source /home/ekoch/preload.bash
+
+# Give which spw using the --export flag when calling sbatch
+export scratch_path=/home/ekoch/scratch/M31_imaging/11B-124/
+
+# export project_path=/home/ekoch/project/ekoch/
+
+# Move to scratch space b/c casa write out the temporary files into the same folder
+cd $scratch_path
+
+Xvfb :1 &
+export DISPLAY=:1
+
+echo "OMP_NUM_THREADS "$OMP_NUM_THREADS
+echo "Running SPW "$spw
+
+$HOME/casa-release-5.4.1-32.el7/bin/mpicasa -n 32 $HOME/casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/LocalGroup-VLA/11B-124/imaging/line_imaging_stage1.py
+
+# Copy the dirty_cube folder into project space
+# cp -R $scratch_path/dirty_cube $project_path
