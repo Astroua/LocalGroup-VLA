3445fb69a79c17b4d1000031b566a6f25ba72a1f
e-koch <koch.eric.w@gmail.com>
2019-05-08 16:45:03 -0600

    Load in spw_dict first

--- 14A-235/imaging/test_line_imaging.py
+++ 14A-235/imaging/test_line_imaging.py
@@ -25,15 +25,15 @@ if not os.path.exists(output_path):
 # Set channel widths to make dirty cubes with
 chan_width = {0: 10, 1: 5, 2: 5, 3: 5, 4: 5}
 
+# Load in the SPW dict in the repo on cedar
+execfile(os.path.expanduser("~/code/LocalGroup-VLA/14A-235/spw_setup.py"))
+
 # Don't image the channel edges
 pad_chan = int(np.ceil(linespw_dict[spw_num][2] * 0.05))
 num_chan = int((linespw_dict[spw_num][2] - 2 * pad_chan) / chan_width[spw_num])
 
 # Grab all of the MS tracks in the folder (should be 17)
 myvis = "14A-235_lines.ms"
-
-# Load in the SPW dict in the repo on cedar
-execfile(os.path.expanduser("~/code/LocalGroup-VLA/14A-235/spw_setup.py"))
 
 # Assume we can set reasonable image parameters from any of the tracks
 mycellsize = set_cellsize(myvis, spw_num, sample_factor=6.,
