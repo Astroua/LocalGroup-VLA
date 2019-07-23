be20d8e2442b7bd3bf5e48f50e2aa4ee404eff4e
e-koch <koch.eric.w@gmail.com>
2019-07-23 10:01:53 -0600

    Add HI channels for contsub

--- 15A-175/spw_setup.py
+++ 15A-175/spw_setup.py
@@ -2,18 +2,17 @@
 # SPW $: [Name, Restfreq, Num chans, Chans for uvcontsub]
 
 # Notes for uvcontsub channels:
-# UPDARE
+# UPDATE
 
 # Channel offset b/w cubes made with test_line_imaging.py (excludes edge
 # channels) and the full SPW (since we split from the full SPW):
 # OH - UPDATE channels
 # HI - 205 channels
 
-# HI velocity from 66 to 795 km/s
-# Use common range for all targets since M31 (at least in the HI) has some
-# emission in the dwarf pointings (mostly NGC 205)
+# HI uvcontsub velocity from 108 to 460 km/s and -610 to -1000 km/s
+# HI emission from 10 to -320.
 
-linespw_dict = {0: ["HI", "1.420405752GHz", 4096, "XXX235~2005"],
+linespw_dict = {0: ["HI", "1.420405752GHz", 4096, "205~1075;2805~3750"],
                 1: ["OH1612", "1.612231GHz", 512, "XXX66~266"],  # Currently all flagged
                 2: ["OH1665", "1.6654018GHz", 512, "XXX66~266"],
                 3: ["OH1667", "1.667359GHz", 512, "XXX66~266"],
