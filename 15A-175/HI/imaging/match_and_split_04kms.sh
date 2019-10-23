3566fdc483a15bb4e85978741a6e74cbd43edf12
e-koch <koch.eric.w@gmail.com>
2019-10-22 19:10:01 -0600

    Update to local run example

--- 15A-175/HI/imaging/match_and_split_04kms.sh
+++ 15A-175/HI/imaging/match_and_split_04kms.sh
@@ -55,7 +55,7 @@ mkdir ${out_chan_folder}
 casa-release-5.4.1-32.el7/bin/mpicasa -n 32 casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/code/LocalGroup-VLA/15A-175/HI/imaging/match_and_split.py True 0.42 $job_num 10 ${scratch_path}/${out_chan_folder}/
 
 # Local run example
-# ~/casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/ownCloud/code_development/LocalGroup-VLA/15A-175/HI/imaging/match_and_split.py True 0.42 0 10 .
+# ~/casa-release-5.4.1-32.el7/bin/casa --nologger --nogui --log2term --nocrashreport -c $HOME/ownCloud/code_development/LocalGroup-VLA/15A-175/HI/imaging/match_and_split.py False 0.42 0 10 .
 
 # echo "Copy back to scratch"
 # cp -r ${out_chan_folder}/* ${scratch_path}/${out_chan_folder}/
