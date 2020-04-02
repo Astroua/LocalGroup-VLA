d0a6eb8f48ab309c3c40e4dc6c777d65e5b7ec52
e-koch <koch.eric.w@gmail.com>
2020-04-02 15:21:42 -0600

    Small updates when regridding the 14A MW HI mask

--- 15A-175/HI/analysis/regrid_14A_MW_HI_mask.py
+++ 15A-175/HI/analysis/regrid_14A_MW_HI_mask.py
@@ -5,10 +5,13 @@ higher sensitivity and should be directly applicable for
 the higher-res 15A cubes.
 '''
 
+
+import os
+from os.path import join as osjoin
+
 from cube_analysis.reprojection import reproject_cube
 
 from paths import (fifteenA_HI_BCtaper_04kms_data_wEBHIS_path,
-                   fifteenA_HI_BC_1_2kms_data_wEBHIS_path,
                    fourteenA_HI_data_wEBHIS_path)
 
 
@@ -32,19 +35,4 @@ reproject_cube(custom_mask_name,
 
 # And regrid for the high-res cube.
 
-custom_mask_name=fourteenA_HI_data_wEBHIS_path("M31_14A_HI_contsub_width_04kms.image.pbcor.EBHIS_feathered_interactive_mask.fits")
 
-targ_cube = fifteenA_HI_BC_1_2kms_data_wEBHIS_path("M31_15A_B_C_14A_HI_contsub_width_1_2kms.image.pbcor.EBHIS_feathered.fits")
-
-out_path = fifteenA_HI_BC_1_2kms_data_wEBHIS_path("",
-                                                  no_check=True)
-reproject_cube(custom_mask_name,
-               targ_cube,
-               "M31_15A_interactive_mask.fits",
-               output_folder=out_path,
-               save_spectral=False,
-               is_huge=True,
-               reproject_type='all',
-               common_beam=False,
-               verbose=True,
-               chunk=40)
