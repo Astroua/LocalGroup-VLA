9434104da22a52485df452858449de37c6ec3efb
e-koch <koch.eric.w@gmail.com>
2020-04-13 14:03:11 -0600

    Added some tests on how the uv-overlap ratios are affected by different choices. Still no clear answer

--- 13A-213/HI/imaging/postprocess_dwarf_cubes.py
+++ 13A-213/HI/imaging/postprocess_dwarf_cubes.py
@@ -3,6 +3,7 @@
 Run the postprocessing on the image cubes:
 (1) Estimate noise levels
 (2) Reproject the single dish data
+(3)
 (3) Run feathering statistics
 (4) Run feathering
 (5) Convert to K
@@ -55,9 +56,11 @@ noise_dict = {}
 sc_factor_dict = {}
 
 num_cores = 1
+overwrite = False
 
 pblim = 0.2
-pblim_feathcompare = 0.5
+# pblim_feathcompare = 0.5
+pblim_feathcompare = 0.2
 hi_freq = 1.42040575177 * u.GHz
 
 # All of these tracks are C or D and have the same
@@ -100,6 +103,11 @@ for gal in gals:
         cube_name = cube_name[0]
         log.info(f"Found cube name: {cube_name}")
 
+        cube_name_nopb = glob(os.path.join(img_dir, "*image.fits"))
+        assert len(cube_name_nopb) == 1
+        cube_name_nopb = cube_name_nopb[0]
+        log.info(f"Found cube name: {cube_name_nopb}")
+
         pb_name = glob(os.path.join(img_dir, "*pb.fits"))
         assert len(pb_name) == 1
         pb_name = pb_name[0]
@@ -111,6 +119,7 @@ for gal in gals:
         log.info(f"Using SD cube {sd_cube_name}")
 
         vla_cube = SpectralCube.read(cube_name)
+        vla_cube_nopb = SpectralCube.read(cube_name_nopb)
 
         pb_cube = SpectralCube.read(pb_name)
 
@@ -148,6 +157,9 @@ for gal in gals:
         sd_cube_jybm_name = f"{sd_cube_name_base}_{img_dir_tag}_Jybm.fits"
         sd_cube_reproj_name = f"{sd_cube_name_base}_{img_dir_tag}_fullreproj_Jybm.fits"
 
+        if overwrite:
+            os.system(f"rm {sd_cube_reproj_name.rstrip('.fits')}*")
+
         if not os.path.exists(sd_cube_reproj_name):
 
             sd_cube = SpectralCube.read(sd_cube_name)
@@ -166,6 +178,8 @@ for gal in gals:
             reproject_cube(sd_cube_jybm_name, cube_name,
                            sd_cube_reproj_name,
                            reproject_type='all',
+                           reproject_alg='interp',
+                           reproject_order='bilinear',
                            chunk=30,
                            wcs_check=False)
 
@@ -181,6 +195,9 @@ for gal in gals:
 
         sd_cube = SpectralCube.read(sd_cube_reproj_name)
 
+        # TODO: registration test with good channels.
+        print("HEY YOU SHOULD ADD CUBE REGISTRATION HERE.")
+
         # Smoothly taper along the pblim threshold
         # This is set really high on purpose, where the VLA data
         # is closest to the pointing centre
@@ -191,7 +208,7 @@ for gal in gals:
                                20, nsig_cut=5)
 
         radii, ratios, high_pts, low_pts, chan_out = \
-            feather_compare_cube(vla_cube, sd_cube, las,
+            feather_compare_cube(vla_cube_nopb, sd_cube, las,
                                  num_cores=num_cores,
                                  chunk=100,
                                  verbose=False,
@@ -199,8 +216,8 @@ for gal in gals:
                                  relax_spectral_check=False,
                                  spec_check_kwargs={'rtol': 0.03})
 
-        sc_factor, sc_err = find_scale_factor(np.hstack(low_pts[mask_chans]),
-                                              np.hstack(high_pts[mask_chans]),
+        sc_factor, sc_err = find_scale_factor(np.array(low_pts)[mask_chans].ravel(),
+                                              np.array(high_pts)[mask_chans].ravel(),
                                               method='distrib',
                                               verbose=True)
 
@@ -237,3 +254,45 @@ for gal in gals:
 
     # Run masking and moment pipeline
     # Enable converting to K
+
+# 1st order interpolation
+# {'IC1613_full_imaging_Cconfig_robust0_noSD': [0.8246755180570601,
+#  0.013512457787146378],
+# 'IC1613_full_imaging_noSD': [0.7877903229234383, 0.005944083885376091],
+# 'SextansA_full_imaging_CDtaper_noSD': [1.0923815618367467,
+#  0.0022907995968346785],
+# 'SextansA_full_imaging_noSD': [1.0992039343194373, 0.0022545850390344115],
+# 'SextansA_full_imaging_Cconfig_noSD': [1.5118005329553237,
+#  0.004158306747845005],
+# 'SextansA_full_imaging_Cconfig_robust0_noSD': [1.7103087300800541,
+#  0.007458281790533004],
+# 'WLM_full_imaging_robust0_noSD': [0.4061674361342547, 0.0014785379845224586],
+# 'WLM_full_imaging_noSD': [0.34200798299651775, 0.000831513578860124]}
+
+# With exact reprojection
+# {'IC1613_full_imaging_Cconfig_robust0_noSD': [0.8087895202213341,
+#   0.01322549385991963],
+#  'IC1613_full_imaging_noSD': [0.7719067042712148, 0.005799565379540013],
+#  'SextansA_full_imaging_CDtaper_noSD': [1.055334181887838,
+#   0.0022587213606174083],
+#  'SextansA_full_imaging_noSD': [1.0615822419669458, 0.0022168885060244604],
+#  'SextansA_full_imaging_Cconfig_noSD': [1.4302942784012236,
+#   0.0034436617923859856],
+#  'SextansA_full_imaging_Cconfig_robust0_noSD': [1.5950500964182992,
+#   0.006379303363553341],
+#  'WLM_full_imaging_robust0_noSD': [0.38536047954215935, 0.0014391435291281399],
+#  'WLM_full_imaging_noSD': [0.32776483984627897, 0.0008016324433172888]}
+
+# interp with non-pb cor. and pblim=0.2
+# {'IC1613_full_imaging_Cconfig_robust0_noSD': [2.455447462117748,
+#   0.0400010978944866],
+#  'IC1613_full_imaging_noSD': [2.0831113364603047, 0.023077620757999327],
+#  'SextansA_full_imaging_CDtaper_noSD': [1.8013629458562161,
+#   0.005986619111690049],
+#  'SextansA_full_imaging_noSD': [1.8231220150768113, 0.005908816806189285],
+#  'SextansA_full_imaging_Cconfig_noSD': [1.6840883653110448,
+#   0.005396704375593885],
+#  'SextansA_full_imaging_Cconfig_robust0_noSD': [1.6058974182643924,
+#   0.0066826033750901044],
+#  'WLM_full_imaging_robust0_noSD': [0.6143887252781745, 0.004742516014397602],
+#  'WLM_full_imaging_noSD': [0.5682772068380337, 0.005067178499413323]}
